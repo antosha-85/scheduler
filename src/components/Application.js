ae6eda6da2d42e6846ec4f899c7ba864b012c7d2
Anton Smirnov <antosha-85@yandex.ru>
2020-02-22 12:01:23 -0700

    Finished first assignment for day 1, modified files, used useApplicationData file

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,63 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import DayList from "components/DayList"
 import "components/Application.scss";
 import Appointment from "components/Appointment";
-import axios from "axios";
 import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 import { getInterviewersForDay } from "../helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
 
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, appointment)
-      .then(() => {
-        setState({
-          ...state,
-          appointments
-        }
-        );
-      })
-  }
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`, appointment)
-      .then(() => {
-        setState({
-          ...state,
-          appointments
-        }
-        );
-      })
-  }
-
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
-
-  const setDay = day => setState({ ...state, day });
-
-  const appointments = getAppointmentsForDay(state, state.day);
-
-  const schedule = appointments.map((appointment) => {
+  const interviewers = getInterviewersForDay(state, state.day);
+  const appointments = getAppointmentsForDay(state, state.day).map((appointment) => {
     const interview = getInterview(state, appointment.interview);
 
     return (
@@ -66,27 +25,13 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
-        interviewers={getInterviewersForDay(state, state.day)}
+        interviewers={interviewers}
         bookInterview={bookInterview}
         cancelInterview={cancelInterview}
 
       />
     );
   });
-
-  useEffect(() => {
-    Promise.all([
-      axios.get(`http://localhost:8001/api/days`),
-      axios.get(`http://localhost:8001/api/appointments`),
-      axios.get(`http://localhost:8001/api/interviewers`)
-    ])
-      .then((all) => {
-        setState(state => ({ ...state, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-      })
-      .catch(err => {
-        console.error('ERRRROROROROR', err)
-      })
-  }, [])
 
   return (
     <main className="layout">
@@ -111,7 +56,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {schedule}
+        {appointments}
         <Appointment key="last" time="5pm" />
       </section>
 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -25,193 +25,195 @@ export function getInterviewersForDay(state, day) {
     if(filteredDays.length === 0) {
         // console.log("TCL: getInterviewersForDay -> finalResult", finalResult)
         return finalResult;
-    }
-    filteredDays = filteredDays[0].interviewers;
-    // console.log("TCL: getInterviewersForDay -> filteredDays", filteredDays)
-    for (const item1 of filteredDays) {
+      }
+      filteredDays = filteredDays[0].interviewers;
+      // console.log("TCL: getInterviewersForDay -> filteredDays", filteredDays)
+      for (const item1 of filteredDays) {
         for (const item2 in state.interviewers) {
-            if (item1 === state.interviewers[item2].id) {
+          if (item1 === state.interviewers[item2].id) {
                 finalResult.push(state.interviewers[item2]);
-            } 
+              } 
         }
-    }
-    // console.log("TCL: getInterviewersForDay -> finalResult", finalResult)
+      }
+      // console.log("TCL: getInterviewersForDay -> finalResult", finalResult)
     return finalResult;
-}
-
-const state = {
-  days: [
-    {
-      id: 1,
-      name: "Monday",
-      appointments: [1, 2, 3],
-      interviewers: [1, 2]
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      appointments: [4, 5],
-      interviewers: [3, 4]
-    }
-  ],
-  appointments: {
-    "1": { id: 1, time: "12pm", interview: null },
-    "2": { id: 2, time: "1pm", interview: null },
-    "3": {
-      id: 3,
-      time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
-    },
-    "4": { id: 4, time: "3pm", interview: null },
-    "5": {
-      id: 5,
-      time: "4pm",
-      interview: { student: "Chad Takahashi", interviewer: 2 }
-    }
-  },
-  interviewers: {
-    "1": {  
-      id: 1,
-      name: "Sylvia Palmer",
-      avatar: "https://i.imgur.com/LpaY82x.png"
-    },
-    "2": {
-      id: 2,
-      name: "Tori Malcolm2",
-      avatar: "https://i.imgur.com/Nmx0Qxo.png"
-    },
-    "3": {
-      id: 3,
-      name: "Tori Malcolm3",
-      avatar: "https://i.imgur.com/Nmx0Qxo.png"
-    },
-    "4": {
-      id: 4,
-      name: "Tori Malcolm4",
-      avatar: "https://i.imgur.com/Nmx0Qxo.png"
-    }
   }
-};
 
-export function getInterview (state, interview) {
-    let finalObj = {};
-    if(!interview) {
-        return null;
-    } else {
-        for (const int in state.interviewers) {
-            if (interview.interviewer === state.interviewers[int].id) {
-                finalObj = {
-                    "student": interview.student,
-                    "interviewer": {
-                        "id": state.interviewers[int].id,
-                        "name": state.interviewers[int].name,
-                        "avatar": state.interviewers[int].avatar
-                    }
-                }
-            }
-        }
-        return finalObj;
-    }
-}
-
-
-
-
-
-getInterview({
-    days: [
-      {
-        id: 1,
-        name: "Monday",
-        appointments: [1, 2, 3]
-      },
-      {
-        id: 2,
-        name: "Tuesday",
-        appointments: [4, 5]
+  export function getInterview (state, interview) {
+      let finalObj = {};
+      if(!interview) {
+          return null;
+      } else {
+          for (const int in state.interviewers) {
+              if (interview.interviewer === state.interviewers[int].id) {
+                  finalObj = {
+                      "student": interview.student,
+                      "interviewer": {
+                          "id": state.interviewers[int].id,
+                          "name": state.interviewers[int].name,
+                          "avatar": state.interviewers[int].avatar
+                      }
+                  }
+              }
+          }
+          return finalObj;
       }
-    ],
-    appointments: {
-      "1": { id: 1, time: "12pm", interview: null },
-      "2": { id: 2, time: "1pm", interview: null },
-      "3": {
-        id: 3,
-        time: "2pm",
-        interview: { student: "Archie Cohen", interviewer: 2 }
-      },
-      "4": { id: 4, time: "3pm", interview: null },
-      "5": {
-        id: 5,
-        time: "4pm",
-        interview: { student: "Chad Takahashi", interviewer: 2 }
-      }
-    },
-    interviewers: {
-      "1": {  
-        "id": 1,
-        "name": "Sylvia Palmer",
-        "avatar": "https://i.imgur.com/LpaY82x.png"
-      },
-      "2": {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      }
-    }
-  }, { student: "Archie Cohen", interviewer: 1 })
+  }
+  //variable for testing
 
-//calling getInterviewersForDay for testing purposes
+  // const state = {
+//   days: [
+//     {
+  //       id: 1,
+//       name: "Monday",
+//       appointments: [1, 2, 3],
+//       interviewers: [1, 2]
+//     },
+//     {
+//       id: 2,
+//       name: "Tuesday",
+//       appointments: [4, 5],
+//       interviewers: [3, 4]
+//     }
+//   ],
+//   appointments: {
+//     "1": { id: 1, time: "12pm", interview: null },
+//     "2": { id: 2, time: "1pm", interview: null },
+//     "3": {
+//       id: 3,
+//       time: "2pm",
+//       interview: { student: "Archie Cohen", interviewer: 2 }
+//     },
+//     "4": { id: 4, time: "3pm", interview: null },
+//     "5": {
+//       id: 5,
+//       time: "4pm",
+//       interview: { student: "Chad Takahashi", interviewer: 2 }
+//     }
+//   },
+//   interviewers: {
+//     "1": {  
+//       id: 1,
+//       name: "Sylvia Palmer",
+//       avatar: "https://i.imgur.com/LpaY82x.png"
+//     },
+//     "2": {
+//       id: 2,
+//       name: "Tori Malcolm2",
+//       avatar: "https://i.imgur.com/Nmx0Qxo.png"
+//     },
+//     "3": {
+//       id: 3,
+//       name: "Tori Malcolm3",
+//       avatar: "https://i.imgur.com/Nmx0Qxo.png"
+//     },
+//     "4": {
+//       id: 4,
+//       name: "Tori Malcolm4",
+//       avatar: "https://i.imgur.com/Nmx0Qxo.png"
+//     }
+//   }
+// };
 
-  getInterviewersForDay ({
-    days: [
-      {
-        id: 1,
-        name: "Monday",
-        appointments: [1, 2, 3],
-        interviewers: [1, 2]
-      },
-      {
-        id: 2,
-        name: "Tuesday",
-        appointments: [4, 5],
-        interviewers: [3, 4]
-      }
-    ],
-    appointments: {
-      "1": { id: 1, time: "12pm", interview: null },
-      "2": { id: 2, time: "1pm", interview: null },
-      "3": {
-        id: 3,
-        time: "2pm",
-        interview: { student: "Archie Cohen", interviewer: 2 }
-      },
-      "4": { id: 4, time: "3pm", interview: null },
-      "5": {
-        id: 5,
-        time: "4pm",
-        interview: { student: "Chad Takahashi", interviewer: 2 }
-      }
-    },
-    interviewers: {
-      "1": {  
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png"
-      },
-      "2": {
-        id: 2,
-        name: "Tori Malcolm2",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      },
-      "3": {
-        id: 3,
-        name: "Tori Malcolm3",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      },
-      "4": {
-        id: 4,
-        name: "Tori Malcolm4",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      }
-    }
-  }, 'Tuesday');
+
+
+
+//funcitons call for testing
+
+// getInterview({
+    // days: [
+      // {
+        // id: 1,
+        // name: "Monday",
+        // appointments: [1, 2, 3]
+      // },
+      // {
+        // id: 2,
+        // name: "Tuesday",
+        // appointments: [4, 5]
+      // }
+    // ],
+    // appointments: {
+      // "1": { id: 1, time: "12pm", interview: null },
+      // "2": { id: 2, time: "1pm", interview: null },
+      // "3": {
+        // id: 3,
+        // time: "2pm",
+        // interview: { student: "Archie Cohen", interviewer: 2 }
+      // },
+      // "4": { id: 4, time: "3pm", interview: null },
+      // "5": {
+        // id: 5,
+        // time: "4pm",
+        // interview: { student: "Chad Takahashi", interviewer: 2 }
+      // }
+    // },
+    // interviewers: {
+      // "1": {  
+        // "id": 1,
+        // "name": "Sylvia Palmer",
+        // "avatar": "https://i.imgur.com/LpaY82x.png"
+      // },
+      // "2": {
+        // id: 2,
+        // name: "Tori Malcolm",
+        // avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      // }
+    // }
+  // }, { student: "Archie Cohen", interviewer: 1 })
+// 
+// calling getInterviewersForDay for testing purposes
+// 
+  // getInterviewersForDay ({
+    // days: [
+      // {
+        // id: 1,
+        // name: "Monday",
+        // appointments: [1, 2, 3],
+        // interviewers: [1, 2]
+      // },
+      // {
+        // id: 2,
+        // name: "Tuesday",
+        // appointments: [4, 5],
+        // interviewers: [3, 4]
+      // }
+    // ],
+    // appointments: {
+      // "1": { id: 1, time: "12pm", interview: null },
+      // "2": { id: 2, time: "1pm", interview: null },
+      // "3": {
+        // id: 3,
+        // time: "2pm",
+        // interview: { student: "Archie Cohen", interviewer: 2 }
+      // },
+      // "4": { id: 4, time: "3pm", interview: null },
+      // "5": {
+        // id: 5,
+        // time: "4pm",
+        // interview: { student: "Chad Takahashi", interviewer: 2 }
+      // }
+    // },
+    // interviewers: {
+      // "1": {  
+        // id: 1,
+        // name: "Sylvia Palmer",
+        // avatar: "https://i.imgur.com/LpaY82x.png"
+      // },
+      // "2": {
+        // id: 2,
+        // name: "Tori Malcolm2",
+        // avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      // },
+      // "3": {
+        // id: 3,
+        // name: "Tori Malcolm3",
+        // avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      // },
+      // "4": {
+        // id: 4,
+        // name: "Tori Malcolm4",
+        // avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      // }
+    // }
+  // }, 'Tuesday');
