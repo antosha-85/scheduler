35573d14d4f0a8f7c7f71ee37509bf29297ca1ae
Anton Smirnov <antosha-85@yandex.ru>
2020-02-24 19:54:57 -0700

    Finished mocking modules with axios, changed http with just api, found an error with proxy - was at a different location

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -38,9 +38,9 @@ export default function useApplicationData(initial) {
   const setDay = day => dispatch({ type: SET_DAY, day })
   useEffect(() => {
     Promise.all([
-      axios.get(`http://localhost:8001/api/days`),
-      axios.get(`http://localhost:8001/api/appointments`),
-      axios.get(`http://localhost:8001/api/interviewers`)
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`)
     ])
       .then((all) => {
         dispatch({ type: SET_APPLICATION_DATA, days: all[0].data, appointments: all[1].data, interviewers: all[2].data });
@@ -71,7 +71,7 @@ export default function useApplicationData(initial) {
       return day
     })
 
-    return axios.put(`http://localhost:8001/api/appointments/${id}`, appointment)
+    return axios.put(`/api/appointments/${id}`, appointment)
       .then(() => {
         dispatch({ type: BOOK_INTERVIEW , appointments, days});
       })
@@ -97,7 +97,7 @@ export default function useApplicationData(initial) {
      return day
    })
 
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`, appointment)
+    return axios.delete(`/api/appointments/${id}`, appointment)
       .then(() => {
         dispatch({ type: CANCEL_INTERVIEW, appointments, days });
       })
